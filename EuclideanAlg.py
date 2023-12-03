d9c1c2759cef54b601cf7ddd486f0a8d42322d22
Alex Taylor <alexetaylor7@gmail.com>
2023-12-03 00:56:34 -0700

    Added methods and class instead of pure user input

--- EuclideanAlg.py
+++ EuclideanAlg.py
@@ -1,27 +1,39 @@
-print("\n")
-print("The Euclidean algorithm is a way to find the greatest common divisor of two positive integers, a and b.")
-print("\n")
 
-# Gather user input
-flag = False
-while not flag:
-    inputOne = input("Enter a non-negative integer for your \"a\" value: ") 
-    inputTwo = input("Enter a non-negative integer for your \"b\" value: ")
-    variables = [inputOne, inputTwo]
+class EuclideanAlg:
+
     print("\n")
-    
-    for var in variables:
-            
-    # Applying error - handling method
-            try:
-                # try converting to integer
-                int(var)
+    print("The Euclidean algorithm is a way to find the greatest common divisor of two positive integers, a and b.")
+    print("\n")
 
-            except ValueError:
-                print("Please enter a valid non-negative integer.")
-                inputOne = input("Enter a non-negative integer for your \"a\" value: ") 
-                inputTwo = input("Enter a non-negative integer for your \"b\" value: ")
-                flag = True
-    # flag check
-    print("GCD(" + inputOne + ", " + inputTwo + ") = ")
-    break
+    # Create a prompt user method
+    def __init__(self) -> None:
+         self.get_user_input()
+         
+
+    # Gather user input
+    def get_user_input(self):
+        flag = False
+        while not flag:
+            inputOne = input("Enter a non-negative integer for your \"a\" value: ") 
+            inputTwo = input("Enter a non-negative integer for your \"b\" value: ")
+            variables = [inputOne, inputTwo]
+            print("\n")
+            
+            for var in variables:
+                    
+            # Applying error - handling method
+                    try:
+                        # try converting to integer
+                        int(var)
+
+                    except ValueError:
+                        print("Please enter a valid non-negative integer.")
+                        inputOne = input("Enter a non-negative integer for your \"a\" value: ") 
+                        inputTwo = input("Enter a non-negative integer for your \"b\" value: ")
+                        flag = True
+                        
+            # flag check
+            print("GCD(" + inputOne + ", " + inputTwo + ") = ")
+            break
+    
+euclidean_instance = EuclideanAlg()
